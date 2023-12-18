22580b46da014ab34877af4a80d2d7c8bfa768b5
Connor Davis <st10068305@vcconnect.edu.za>
2023-12-18 08:52:50 +0200

    Refactor UI initialization and add GUI art

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -3,9 +3,12 @@ use anyhow::Error;
 use super::{gui::Gui, silent_shot::silent_shot_gui};
 
 pub async fn initialize_ui() -> Result<(), Error> {
-    let gui = Gui::init()?;
-
     loop {
+        let gui = Gui::init()?;
+
+        gui.clear();
+        gui.art();
+
         let input = gui.menu(vec!["Silent Shot", "Exit"]);
 
         match input.as_str() {
