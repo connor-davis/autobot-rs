c1c5d2cec7d99b0b23c451c11ab7435a8737d269
Connor Davis <st10068305@vcconnect.edu.za>
2023-12-18 08:52:31 +0200

    Refactor silent_shot_gui function

--- src/ui/silent_shot.rs
+++ src/ui/silent_shot.rs
@@ -1,10 +1,26 @@
-use std::thread::sleep;
-
 use anyhow::Error;
+
+use crate::config::save_config;
 
 use super::gui::Gui;
 
-pub async fn silent_shot_gui(gui: Gui) -> Result<(), Error> {
+pub async fn silent_shot_gui(mut gui: Gui) -> Result<(), Error> {
+    gui.lines(vec![
+        format!("Lethal Key: {}", gui.config.silent_shot_lethal_key),
+        format!(
+            "Weapon Swap Key: {}",
+            gui.config.silent_shot_weapon_swap_key
+        ),
+        format!(
+            "Lethal Key Delay: {}ms",
+            gui.config.silent_shot_lethal_key_delay
+        ),
+        format!(
+            "Weapon Swap Delay: {}ms",
+            gui.config.silent_shot_weapon_swap_delay
+        ),
+    ]);
+
     let input = gui.menu(vec![
         "Edit Lethal Key",
         "Edit Weapon Swap Key",
@@ -17,19 +33,53 @@ pub async fn silent_shot_gui(gui: Gui) -> Result<(), Error> {
         "1" => {
             let choice = gui.interactive_input("Enter a new lethal key: ");
 
-            println!("{} has been selected.", choice);
+            if choice.is_empty() {
+                gui.lines(vec!["Invalid input.".to_string()]);
+            } else {
+                gui.config.silent_shot_lethal_key = choice;
 
-            sleep(std::time::Duration::from_millis(2000));
+                save_config(gui.config.clone())?;
+            }
 
             gui.clear();
         }
         "2" => {
+            let choice = gui.interactive_input("Enter a new weapon swap key: ");
+
+            if choice.is_empty() {
+                gui.lines(vec!["Invalid input.".to_string()]);
+            } else {
+                gui.config.silent_shot_weapon_swap_key = choice;
+
+                save_config(gui.config.clone())?;
+            }
+
             gui.clear();
         }
         "3" => {
+            let choice = gui.input("Enter a new lethal key delay: ");
+
+            if choice.is_empty() {
+                gui.lines(vec!["Invalid input.".to_string()]);
+            } else {
+                gui.config.silent_shot_lethal_key_delay = choice.parse::<u64>()?;
+
+                save_config(gui.config.clone())?;
+            }
+
             gui.clear();
         }
         "4" => {
+            let choice = gui.input("Enter a new weapon swap delay: ");
+
+            if choice.is_empty() {
+                gui.lines(vec!["Invalid input.".to_string()]);
+            } else {
+                gui.config.silent_shot_weapon_swap_delay = choice.parse::<u64>()?;
+
+                save_config(gui.config.clone())?;
+            }
+
             gui.clear();
         }
         "5" => {
